8aab21a53ce3eb41df7fac945f38f1971582bb55
d-beloved <morayodeji@gmail.com>
2020-12-22 14:46:38 +0100

    filter entries

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TransactionTable({txns}) {
+  const [dateOfTrnsct, setDateOfTrnsct] = useState("");
+
+
   const sort = () => {};
+
+  const changeDate = (e) => {
+    setDateOfTrnsct( e.target.value )
+  };
+
+  const handleTableData = (e) => {
+    console.log('I got here')
+    return (
+      (dateOfTrnsct === "") ? (
+        txns.map((txn, index) => (
+          <tr key={index}>
+            <td>{txn.date}</td>
+            <td>{txn.description}</td>
+            <td>{txn.type === 1 ? "Debit" : "Credit"}</td>
+            <td>{txn.amount}</td>
+            <td>{txn.balance}</td>
+          </tr>
+        ))
+      ) : (
+        txns.filter(txn => txn.date === dateOfTrnsct).map((filteredTxns, index) => (
+          <tr key={index}>
+            <td>{filteredTxns.date}</td>
+            <td>{filteredTxns.description}</td>
+            <td>{filteredTxns.type === 1 ? "Debit" : "Credit"}</td>
+            <td>{filteredTxns.amount}</td>
+            <td>{filteredTxns.balance}</td>
+          </tr>
+      )))
+    )
+  };
 
   return (
     <div className="layout-column align-items-center mt-50">
       <section className="layout-row align-items-center justify-content-center">
         <label className="mr-10">Transaction Date</label>
-        <input id="date" type="date" defaultValue="2019-11-29" onChange="" />
-        <button className="small">Filter</button>
+        <input id="date" type="date" onChange={changeDate} name="date" defaultValue="2019-11-29"/>
+        <button className="small" onClick="">Filter</button>
       </section>
 
       <div className="card mt-50">
@@ -27,15 +60,7 @@ function TransactionTable({txns}) {
             </tr>
           </thead>
           <tbody>
-            {txns.map((txn, index) => (
-              <tr key={index}>
-                <td>{txn.date}</td>
-                <td>{txn.description}</td>
-                <td>{txn.type === 1 ? "Debit" : "Credit"}</td>
-                <td>{txn.amount}</td>
-                <td>{txn.balance}</td>
-              </tr>
-            ))}
+            {handleTableData()}
           </tbody>
         </table>
       </div>
